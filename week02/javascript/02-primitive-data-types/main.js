756ce32432c486cd3c4acac2d6a5e83426a6a11d
yu-rim98 <rimmm098@gmail.com>
2025-09-17 10:03:54 +0900

    원시 자료형 - 숫자형(Number)

--- week02/javascript/02-primitive-data-types/main.js
+++ week02/javascript/02-primitive-data-types/main.js
@@ -9,3 +9,16 @@ console.log(typeof "Hello World"); // string
 
 // 빈 문자열("")도 문자열이다.
 console.log(typeof ""); // stirng
+
+// 2. 숫자형 (Number)
+// 모든 종류의 숫자 (정수, 0, 실수)
+console.log(10);
+console.log(0);
+console.log(-1);
+console.log(1.1);
+
+// 10, 0, -1, 1.1 자료형 log
+console.log(typeof 10);
+console.log(typeof 0);
+console.log(typeof -1);
+console.log(typeof 1.1);
