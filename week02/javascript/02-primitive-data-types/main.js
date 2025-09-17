9cf425ab1beb4d9ca85c7bf6652ce8d823d24d71
yu-rim98 <rimmm098@gmail.com>
2025-09-17 10:01:02 +0900

    원시 자료형 - 문자열(string)

--- week02/javascript/02-primitive-data-types/main.js
+++ week02/javascript/02-primitive-data-types/main.js
@@ -2,3 +2,10 @@
 
 // typeof : 데이터의 자료형을 확인
 console.log(typeof "Hello World"); // string
+
+// 원시 자료형 5개 (원래 7개)
+// 1. 문자열 (string)
+// 문자 (글자)의 나열 : 0개 이상의 문자를 나타내는 자료형
+
+// 빈 문자열("")도 문자열이다.
+console.log(typeof ""); // stirng
