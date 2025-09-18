be3941ab266b70e742a23b50f21a7debd059594e
yu-rim98 <rimmm098@gmail.com>
2025-09-18 10:59:56 +0900

    중첩 조건문

--- week02/javascript/06-condition/main.js
+++ week02/javascript/06-condition/main.js
@@ -58,3 +58,41 @@ if (number2 > 0) {
 } else {
   console.log("0");
 }
+
+// 중첩 조건문
+// 조건문 내부에 조건문을 중첩하는 조건문
+
+// 변수 score를 선언하고, 숫자 75를 할당한다
+// 만약 변수 socre가 90 이상이라면
+// 그런데 만약 변수 score가 90 미만 그리고(&&), 80 이상이라면
+// 그런데 만약 변수 score가 80 미만 그리고(&&), 70 이상이라면
+// 그런데 만약 변수 score가 70 미만 그리고(&&), 60 이상이라면
+// 그런데 모두 아니라면
+
+console.log("중첩 조건문");
+
+let score = 75;
+
+if (score >= 90) {
+  console.log("90 이상");
+} else if (score < 90 && score >= 80) {
+  console.log("90 미만 80 이상");
+} else if (score < 80 && score >= 70) {
+  console.log("80 미만 70 이상");
+} else if (score < 70 && score >= 60) {
+  console.log("70 미만 60 이상");
+} else {
+  console.log("모두 아님");
+}
+
+if (score >= 90) {
+  console.log("90 이상");
+} else if (score >= 80) {
+  console.log("90 미만 80 이상");
+} else if (score >= 70) {
+  console.log("80 미만 70 이상");
+} else if (score >= 60) {
+  console.log("70 미만 60 이상");
+} else {
+  console.log("모두 아님");
+}
