a5d779c9aa06fda6b452f7c27c09c039655f0b31
yu-rim98 <rimmm098@gmail.com>
2025-09-18 09:59:17 +0900

    조건문 - if ~ else if

--- week02/javascript/06-condition/main.js
+++ week02/javascript/06-condition/main.js
@@ -7,3 +7,21 @@
 if (5 > 1) {
   console.log("5는 1보다 큼");
 }
+
+/* if ~ else if 조건문 기본 구조
+  if (조건식) {
+    참 일 때 실행
+  } else if (조건식) { // if 조건식이 거짓일 때
+    참 일 때 실행
+  }
+*/
+
+if (5 > 10) {
+  console.log("5는 10보다 큼");
+} else if (5 > 7) {
+  console.log("5는 7보다 큼");
+} else if (5 > 5) {
+  console.log("5는 5보다 큼");
+} else if (5 > 3) {
+  console.log("5는 3보다 큼");
+}
