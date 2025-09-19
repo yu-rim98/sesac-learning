af68a1bb1c64fa778490247e261e4b3c9e4d8e86
yu-rim98 <rimmm098@gmail.com>
2025-09-19 09:27:33 +0900

    함수 선언과 호출

---
+++ week02/javascript/08-function/main.js
@@ -0,0 +1,13 @@
+// 두 숫자를 매개변수로 받아서 더한 숫자를 반환하는 함수
+
+// 매개변수가 3개인 함수
+function add(number1, number2, number3) {
+  // 함수 호출 시 실행될 코드 블럭
+  // 숫자 3개를 매개변수로 받아 더한 값을 반환하는 함수
+  return number1 + number2 + number3; // 반환
+}
+
+// 함수 호출
+const result = add(10, 10, 10);
+
+console.log(result);
