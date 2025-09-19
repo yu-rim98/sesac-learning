2ac76853d58e68460c77cd15cade25d112d15c36
yu-rim98 <rimmm098@gmail.com>
2025-09-19 13:57:48 +0900

    배열 생성

---
+++ week02/javascript/09-array/main.js
@@ -0,0 +1,13 @@
+// 배열 생성
+// 대괄호 [] 사용
+
+// 빈 배열 데이터를 변수 emptyArray 할당
+let emptyArray = [];
+
+// 1을 저장한 배열 데이터를 변수 oneArray 할당
+let oneArray = [1];
+
+// 원소를 여러개 저장하기 위해서는 , 로 구분한다.
+// 1, 2, 3, 4 를 저장한 배열 데이터를 변수 manyArray 할당
+
+let manyArray = [1, 2, 3, 4];
