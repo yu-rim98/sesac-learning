6b4ce8cb4464badc0023fb08b3bd5ad6b6e558e9
yu-rim98 <rimmm098@gmail.com>
2025-09-22 15:08:48 +0900

    배열 고차 메서드
    자바스크립트에서는 배열에 대해 유용한 고차 메서드들을 미리 만들어두었다.
    
    forEach(), map(), filter() 등

---
+++ week03/javascript/14-callback/main.js
@@ -0,0 +1,32 @@
+// 배열 고차 메서드
+// 배열 원소에 콜백 함수를 순차적으로 적용하는 메서드를 의미한다.
+
+// 입렵값에 * 2 반환하는 함수
+function multiTwo(value) {
+  return value * 2;
+}
+
+function higherOrderFunction(array, callbackFunction) {
+  for (let element of array) {
+    const result = callbackFunction(element);
+    console.log(result);
+  }
+}
+
+numbers = [1, 2, 3, 4, 5];
+
+// 콜백 함수를 인자로 전달
+higherOrderFunction(numbers, multiTwo);
+
+console.log("=======");
+
+// 화살표 함수 전달 (콜백 함수)
+higherOrderFunction(numbers, (value) => {
+  return value + 1;
+});
+
+console.log("=======");
+
+higherOrderFunction(numbers, (value) => {
+  return value * value;
+});
