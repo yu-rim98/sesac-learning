87d4b5368b07e7b5e155afc1aca358b9d1848e29
yu-rim98 <rimmm098@gmail.com>
2025-09-22 13:35:05 +0900

    예외처리 기본 구조

--- week03/javascript/13-exception/main.js
+++ week03/javascript/13-exception/main.js
@@ -1,4 +1,18 @@
-const constVar = "상수";
-constVar = "상수 재할당";
+// 예외처리
+// 프로그램 실행 중 발생하는 오류를 처리하는 기술
+// 예외(오류) 발생 시, 프로그램이 종료되지만 예외 처리를 통해 프로그램 종료 대신 적절한 대처 가능
 
-console.log("오류 발생 이후 코드");
+try {
+  // try 코드 블럭
+  // 오류 발생 가능성이 있는 코드를 작성
+} catch (error) {
+  // catch 코드 블럭
+  // 오류 발생 시 실행할 코드를 작성
+  // error 매개변수를 받음. error 매개변수는 오류의 정보를 가지고 있는 객체이다.
+  console.log(error.type);
+  console.log(error.message);
+} finally {
+  // finally 코드 블럭
+  // 오류 발생 여부와 상관없이 실행할 코드를 작성
+  // 무조건 실행되는 코드
+}
