36acd4ccb2693735100a7a7382d97f223ca1881a
yu-rim98 <rimmm098@gmail.com>
2025-09-23 11:39:16 +0900

    fetch() 활용
    fetch()는 브라우저나 Node.js에 기본 내장된 함수이다.
    
    내부적으로 이미 Promise를 반환하도록 설계 되어 있기 때문에 따로 new Promise()를 만들 필요가 없음

--- week03/javascript/16-promise/main.js
+++ week03/javascript/16-promise/main.js
@@ -2,18 +2,18 @@
 // new Promise() : 생성자 함수
 // (resolve, reject) => {} : 비동기 작업을 수행하고, 결과를 반환하는 함수
 
-const promise = new Promise((resolve, reject) => {
-  // 비동기 작업 흉내
-  setTimeout(() => {
-    const random = Math.random(); // 랜덤 숫자 생성
+// const promise = new Promise((resolve, reject) => {
+//   // 비동기 작업 흉내
+//   setTimeout(() => {
+//     const random = Math.random(); // 랜덤 숫자 생성
 
-    if (random > 0.5) {
-      resolve("숫자가 0.5 이상! 성공"); // 성공 시 promise의 콜백 함수로 반환
-    } else {
-      reject("숫자가 0.5 이하! 실패"); // 실패 시 promise의 콜백 함수로 반환
-    }
-  }, 1000);
-});
+//     if (random > 0.5) {
+//       resolve("숫자가 0.5 이상! 성공"); // 성공 시 promise의 콜백 함수로 반환
+//     } else {
+//       reject("숫자가 0.5 이하! 실패"); // 실패 시 promise의 콜백 함수로 반환
+//     }
+//   }, 1000);
+// });
 
 // Promise 상태
 // 1. Pending (대기) : 비동기 작업이 아직 끝나지 않은 초기 상태
@@ -25,15 +25,27 @@ const promise = new Promise((resolve, reject) => {
 // 실패 상태일 때 수행할 처리 메서드 : Promise.catch(콜백함수)
 // 성공/실패와 관계없이 항상 수행할 처리 메서드 : Promise.catch(콜백함수)
 
-promise
-  .then((result) => {
-    // resolve 함수의 반환값을 매개변수로 받는다.
-    console.log(`성공 : ${result}`);
+// promise
+//   .then((result) => {
+//     // resolve 함수의 반환값을 매개변수로 받는다.
+//     console.log(`성공 : ${result}`);
+//   })
+//   .catch((error) => {
+//     // reject 함수의 반환값을 매개변수로 받는다.
+//     console.log(`실패 : ${error}`);
+//   });
+// // .finally(() => {
+// //   console.log("성공/실패 여부와 관계 없이 항상 실행");
+// // });
+
+// fetch : 네트워크 통신 함수 (promise 기반)
+// 필수 요소 : 통신하고자 하는 웹 서비스의 주소
+fetch("https://example.com")
+  .then((response) => {
+    console.log(response);
+    return response.text();
   })
-  .catch((error) => {
-    // reject 함수의 반환값을 매개변수로 받는다.
-    console.log(`실패 : ${error}`);
+  .then((data) => {
+    // Promise 체이닝 - hen() 함수를 여러 번 사용하여 여러 비동기 작업을 순차적으로 처리한다.
+    console.log(data);
   });
-// .finally(() => {
-//   console.log("성공/실패 여부와 관계 없이 항상 실행");
-// });
