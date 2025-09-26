1105241b971c11fdb74ef118d9fa7db05f62e931
yu-rim98 <rimmm098@gmail.com>
2025-09-26 09:56:11 +0900

    일반 함수와 화살표 함수의 this 차이점
    ** 일반 함수에서 this는 함수가 호출될 때 바인딩됨 **
    ** 화살표 함수에서 this는 함수 선언 시점에 상위 스코프 this를 물려받음 **

--- week03/javascript/23-this/main.js
+++ week03/javascript/23-this/main.js
@@ -40,5 +40,32 @@ const user2 = {
   },
 };
 
+// greet는 화살표 함수이기 때문에 user2 객체의 this를 가리키지 않는다.
+// 화살표 함수는 선언되었을 때 상위 스코프 this를 물려받는다,
+// 즉, 해당 화살표 함수는 전역 스코프에서 정의된 것과 같다.
+// 1. this -> 전역 객체(window or global)
+// 2. 호출 주체는 user2이지만 화살표 함수는 전역 객체를 가리키고 있음
 user2.greet();
 console.log(this);
+
+// 객체 메서드 내에서의 화살표 함수
+const user3 = {
+  name: "홍길동",
+  age: 30,
+  greet: function () {
+    const arrowFunc = () => {
+      console.log(this);
+    };
+
+    arrowFunc();
+  },
+};
+
+// greet는 일반 함수이고 호출 주체는 user3 객체이다.
+// 따라서 greet 안의 this는 user3 객체를 가리킨다.
+// arrowFunc는 greet 내부에서 선언된 화살표 함수이며 해당 this는 greet this를 물려받는다.
+
+// 1. this -> user3
+// 2. 화살표 함수이지만 this는 바깥에서 받음 (greet)
+// 3. user3.gree() 호출 -> greet 내부의 this는 user3을 가리킴 -> 화살표 함수에게 물려줌
+user3.greet();
