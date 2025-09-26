64561105d03a41020be2864811fb0d016ecfdc24
yu-rim98 <rimmm098@gmail.com>
2025-09-26 10:35:05 +0900

    스프레드 연산자 (얕은 복사)
    스프레드 연산자는 객체 or 배열의 값을 펼친다.
    
    {}, [] 리터럴이 새로 객체나 배열을 생성해주는 것임

--- week03/javascript/24-modern/spread.js
+++ week03/javascript/24-modern/spread.js
@@ -16,3 +16,13 @@ obj2.location = "대구";
 // obj1과 obj2는 같은 객체를 참조하고 있기 때문에 obj2의 속성을 수정하면 obj1도 같이 수정됨
 console.log(obj1);
 console.log(obj2);
+
+// 스프레드 연산자를 활용한 복사
+const obj3 = { ...obj1 }; // obj1 객체 내부의 값을 펼치고 {} 리터럴로 객체가 새로 생성된다.
+console.log(obj1);
+console.log(obj3);
+
+obj3.name = "고길동";
+
+console.log(obj1);
+console.log(obj3);
