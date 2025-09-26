55468a7dc4ee48ff67871ac400a3e682f28801fb
yu-rim98 <rimmm098@gmail.com>
2025-09-26 13:33:36 +0900

    정의되지 않 undefined에 접근하려하면 에러 발생함

---
+++ week03/javascript/24-modern/optional.js
@@ -0,0 +1,10 @@
+const user = {
+  name: "홍길동",
+};
+
+// address는 user 객체의 속성에 포함되지 않음
+console.log(user.address); // undefined
+
+// 정의되지 않은 undefined에 접근하려하니 에러 발생함
+// TypeError: Cannot read properties of undefined (reading 'city')
+console.log(user.address.city);
