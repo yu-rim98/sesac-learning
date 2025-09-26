c1e81fedd594cad74e1ab3aa8ee3a29b7ad84538
yu-rim98 <rimmm098@gmail.com>
2025-09-26 09:27:00 +0900

    this 사용전 : 객체의 메서드에 인자로 이름과 나이를 전달하고 메서드는 매개변수를 받아서 출력한다.

--- week03/javascript/23-this/main.js
+++ week03/javascript/23-this/main.js
@@ -20,3 +20,22 @@ const obj = {
 
 // 함수를 실행한 객체를 가리킨다.
 obj.func(); // obj를 가리킴
+
+const user = {
+  name: "고길동",
+  age: 20,
+  greet: function (name, age) {
+    console.log(`제 이름은 ${name}이고 나이는 ${age} 입니다.`);
+  },
+};
+
+user.greet("고길동", "20");
+
+// 화살표 함수로 표현한 메서드는 함수 선언식(function 키워드를 사용한 함수)과 작동 방식이 다르다.
+const user2 = {
+  name: "홍길동",
+  age: 30,
+  greet: () => {
+    console.log(this);
+  },
+};
