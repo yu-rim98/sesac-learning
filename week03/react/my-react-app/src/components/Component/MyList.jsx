4cef937dfbc8ab9cd5afe5733480c312da48f326
yu-rim98 <rimmm098@gmail.com>
2025-09-26 17:20:21 +0900

    컴포넌트 생성

---
+++ week03/react/my-react-app/src/components/Component/MyList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MyList() {
+  return (
+    <ul>
+      <li>1. 리액트 프로젝트 생성</li>
+      <li>2. 터미널 경로 이동</li>
+      <li>3. 의존성 설치</li>
+      <li>4. 프로젝트 서버 실행</li>
+    </ul>
+  );
+}
