1e12fa9f2c397cf0802f28eefcff5321f336c4d8
yu-rim98 <rimmm098@gmail.com>
2025-09-26 13:34:28 +0900

    옵셔널 체이닝을 통해 오류 발생하지 않도록 한다.

--- week03/javascript/24-modern/optional.js
+++ week03/javascript/24-modern/optional.js
@@ -7,4 +7,7 @@ console.log(user.address); // undefined
 
 // 정의되지 않은 undefined에 접근하려하니 에러 발생함
 // TypeError: Cannot read properties of undefined (reading 'city')
-console.log(user.address.city);
+// console.log(user.address.city);
+
+// 옵셔널 체이닝(Optional Chaining) 사용
+console.log(user?.address?.city);
