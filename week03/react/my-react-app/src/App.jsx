8a612c5a5949ecc7651dbe22e0b021b5081ef69d
yu-rim98 <rimmm098@gmail.com>
2025-09-26 16:03:27 +0900

    default export (기본 내보내기) - 모듈 당 하나의 컴포넌트 함수만 내보낸다.
    컴포넌트 함수는 하나의 HTML 요소를 반환해야 한다. (돔 트리 구조)

--- week03/react/my-react-app/src/App.jsx
+++ week03/react/my-react-app/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+// default export (기본 내보내기)
+// 파일(모듈) 내 하나의 컴포넌트 함수만 내보낼 수 있다.
+// 컴포넌트 함수는 하나의 HTML 요소(Element)를 반환해야 한다.
 export default function App() {
-  return <h1 className="font-bold text-red-900">App</h1>;
+  // div 요소 3개 반환 -> 하나의 요소만 반환해야하므로 에러
+  // return <div>App</div><dvi>2</dvi> <div>2</div>;
+  return <div>App</div>;
 }
