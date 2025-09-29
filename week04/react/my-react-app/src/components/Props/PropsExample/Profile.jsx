ce7d71e0cb141cffef6272cc5df5107d776f48f0
yu-rim98 <rimmm098@gmail.com>
2025-09-29 15:48:27 +0900

    props 변수 전달

--- week04/react/my-react-app/src/components/Props/PropsExample/Profile.jsx
+++ week04/react/my-react-app/src/components/Props/PropsExample/Profile.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Profile = (props) => {
   return (
     <div>
-      저는 {props.user.name}이고, {props.user.age}세 이며, 관리자 여부는
-      {String(props.user.isAdmin)}
+      저는 {props.name}이고, {props.age}세 이며, 관리자 여부는
+      {String(props.isAdmin)}
       입니다.
     </div>
   );
--- week04/react/my-react-app/src/components/Props/PropsExample/ProfileContainer.jsx
+++ week04/react/my-react-app/src/components/Props/PropsExample/ProfileContainer.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import Profile from "./Profile";
 
 const ProfileContainer = () => {
+  const name = "현우";
+  const age = 22;
+  const isAdmin = true;
   return (
     <div>
       {/* <Profile name="현우" age={22} isAdmin={true} /> */}
-      <Profile user={{ name: "현우", age: 22, isAdmin: true }} />
+      <Profile name={name} age={age} isAdmin={isAdmin} />
       {/* <Profile name="수진" age={21} isAdmin={false} /> */}
     </div>
   );
