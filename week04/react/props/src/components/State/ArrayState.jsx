b29a8c0a0b4d116a67f4f9436e6fa0d71cac14bf
yu-rim98 <rimmm098@gmail.com>
2025-09-30 17:16:44 +0900

    useState(배열) - 배열 요소 삭제하기 filter 활용

--- week04/react/props/src/components/State/ArrayState.jsx
+++ week04/react/props/src/components/State/ArrayState.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 
 const ArrayState = () => {
-  const [array, setArray] = useState([{ id: 1, name: "철수" }]);
+  const [array, setArray] = useState([
+    { id: 1, name: "철수" },
+    { id: 2, name: "영희" },
+    { id: 3, name: "동수" },
+  ]);
 
   const handleAdd = () => {
     const newId = array.length + 1;
@@ -10,13 +14,31 @@ const ArrayState = () => {
     const newArry = [...array, { id: newId, name: newName }];
     setArray(newArry);
   };
+
+  const handleRemove = () => {
+    if (array.length < 1) {
+      alert("삭제할 친구가 없습니다.");
+      return;
+    }
+
+    const lastId = array.length;
+
+    const newArry = array.filter((element) => {
+      if (element.id !== lastId) {
+        return true;
+      }
+    });
+
+    setArray(newArry);
+  };
   return (
     <div>
       <ul>
         {array.map((element) => (
           <li key={element.id}>{element.name}</li>
         ))}
-        <button onClick={handleAdd}>친구 추가</button>
+        <button onClick={handleAdd}>친구 추가</button> <br />
+        <button onClick={handleRemove}>친구 제거</button>
       </ul>
     </div>
   );
