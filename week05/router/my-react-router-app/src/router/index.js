b8e0bf5cdfdd61bb49bac7e992a81f11904bfb6c
yu-rim98 <rimmm098@gmail.com>
2025-10-13 10:24:18 +0900

    라우터 추가

--- week05/router/my-react-router-app/src/router/index.js
+++ week05/router/my-react-router-app/src/router/index.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Profile from "../pages/Profile";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     Component: About,
     // 동시에 쓰면 우선순위가 꼬이거나, React Router의 라이프사이클 처리 로직이 깨짐 - 혼용 X
     // element: <About />, // 이전 버전에서는 컴포넌트 태그 자체를 지정
+  },
+  {
+    path: "/profile",
+    Component: Profile,
   },
 ]);
 
