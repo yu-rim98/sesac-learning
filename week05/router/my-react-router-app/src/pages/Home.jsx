88c63f137efd96e0e52aef6507b43aa790058748
yu-rim98 <rimmm098@gmail.com>
2025-10-13 10:09:13 +0900

    컴포넌트 연결 - Link

--- week05/router/my-react-router-app/src/pages/Home.jsx
+++ week05/router/my-react-router-app/src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom"; // a 태그를 대체하는 컴포넌트
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* to 속성 : 어디로 이동할 건지 지정 - 경로 매핑 */}
+      <Link to="/">홈</Link>
+      <br />
+      <Link to="/about">소개</Link>
+    </div>
+  );
 };
 
 export default Home;
