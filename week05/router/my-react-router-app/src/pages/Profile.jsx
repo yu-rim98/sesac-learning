33478d54bb62f1537fa722a73e53bd2aa45c9e99
yu-rim98 <rimmm098@gmail.com>
2025-10-13 10:39:25 +0900

    useNavigate() : 이벤트 핸들러 또는 useEffect 훅 내부에서 특정 로직 실행 후 페이지를 이동할 때 사용

--- week05/router/my-react-router-app/src/pages/Profile.jsx
+++ week05/router/my-react-router-app/src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Profile = () => {
   // 로그인 상태
-  const isLogin = false;
+  const isLogin = true;
+
+  // useNavigate
+  const navigate = useNavigate();
 
   // 로그인 상태가 아니면 "/"로 리디렉션
   if (!isLogin) {
@@ -11,7 +14,21 @@ const Profile = () => {
     // replace : history(사용자가 접속한 기록)에 남지 않음
     return <Navigate to="/" replace />;
   }
-  return <div>사용자 정보</div>;
+
+  return (
+    <div>
+      사용자 정보
+      <button
+        onClick={() => {
+          alert("홈페이지로 이동합니다.");
+          navigate("/");
+        }}
+        className="border p-2"
+      >
+        홈페이지로 이동
+      </button>
+    </div>
+  );
 };
 
 export default Profile;
