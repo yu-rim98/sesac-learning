67436712522ad3a489acd32fefcc0b1a791926e3
yu-rim98 <rimmm098@gmail.com>
2025-10-15 20:06:46 +0900

    스토어 연결

--- week05/redux/redux-async/src/main.jsx
+++ week05/redux/redux-async/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+
 import router from "./router/index.js";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/index.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
