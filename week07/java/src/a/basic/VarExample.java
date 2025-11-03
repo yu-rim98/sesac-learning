a7ce1c1ad4818e6b65af66ff7f62515b721dd7ed
yu-rim98 <rimmm098@gmail.com>
2025-11-03 11:53:49 +0900

    var - 타입 추론

---
+++ week07/java/src/a/basic/VarExample.java
@@ -0,0 +1,13 @@
+package a.basic;
+
+public class VarExample {
+
+    public static void main(String[] args) {
+        // 타입 추론 : 타입을 추론하고 고정함
+        var age = 20;
+        var myDate1 = true;
+        var myDate2 = 100;
+
+        // myDate1 = "hi"; 불가함
+    }
+}
