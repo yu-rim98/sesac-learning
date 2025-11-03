577dbb1f65f3bdf15efc50fdc884a344dac226d4
yu-rim98 <rimmm098@gmail.com>
2025-11-03 10:46:12 +0900

    출력 예제

---
+++ week07/java/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print("world");
+
+        System.out.println("bye");
+        System.out.println("world");
+
+        String name = "kim";
+        System.out.printf("이름 : %s", name);
+    }
+
+}
