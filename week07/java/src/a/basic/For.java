83c6eb9360ea1b23fb5ad8fbb7269c202f17abba
yu-rim98 <rimmm098@gmail.com>
2025-11-04 11:05:50 +0900

    continue

--- week07/java/src/a/basic/For.java
+++ week07/java/src/a/basic/For.java
@@ -45,6 +45,14 @@ public class For {
             }
         }
 
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) { // i가 짝수면 건너뜀
+                continue;
+            }
+
+            System.out.println(i);
+        }
+
     }
 
 }
