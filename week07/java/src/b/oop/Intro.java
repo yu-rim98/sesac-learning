f444fe216775d71eeac96be3734536cc4616c21e
yu-rim98 <rimmm098@gmail.com>
2025-11-05 11:42:44 +0900

    계산 로직을 메서드로 분리해 재사용함
    데이터와 기능이 분리되어 있음 -> 하나로 묶어야 함

--- week07/java/src/b/oop/Intro.java
+++ week07/java/src/b/oop/Intro.java
@@ -3,16 +3,14 @@ package b.oop;
 public class Intro {
 
     public static void main(String[] args) {
-        int width = 100;  // 너비
-        int height = 200;  // 높이
+        int rec1 = calArea(100, 200);
+        System.out.println(rec1);
 
-        int area = width * height;
-        System.out.println(area);
+        int rec2 = calArea(10, 20);
+        System.out.println(rec2);
+    }
 
-        // 다른 사각형을 만들기 위해서는 코드가 중복됨
-        int width2 = 10;
-        int height2 = 20;
-
-        int area2 = width2 * height2;
+    public static int calArea(int width, int height) {
+        return width * height;
     }
 }
