914b96d3d53d494f121fea95c826916f9f1646a0
yu-rim98 <rimmm098@gmail.com>
2025-11-05 14:35:12 +0900

    참조값 복사 주의

--- week07/java/src/b/oop/Main.java
+++ week07/java/src/b/oop/Main.java
@@ -4,10 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(10, 20);
-        Circle c1 = new Circle(10);
-
         Rectangle r2 = new Rectangle(20, 20);
         Rectangle r3 = new Rectangle(100, 100);
+
+        System.out.println(r2.width);
+        System.out.println(r2.area());
+
+        Circle c1 = new Circle(10);
+        Circle c2 = new Circle(20);
+        Circle c3 = c2;
+
+        c1.radius = 100;
+        c2.radius = 200;
+
+        System.out.println(c1.radius);
+        System.out.println(c2.radius);
+        System.out.println(c3.radius);
+
 
     }
 
