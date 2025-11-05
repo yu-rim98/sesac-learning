0df7d19c9b49cac6ac7553478eed40d7459d0fc9
yu-rim98 <rimmm098@gmail.com>
2025-11-05 11:37:32 +0900

    코드 중복

---
+++ week07/java/src/b/oop/Intro.java
@@ -0,0 +1,18 @@
+package b.oop;
+
+public class Intro {
+
+    public static void main(String[] args) {
+        int width = 100;  // 너비
+        int height = 200;  // 높이
+
+        int area = width * height;
+        System.out.println(area);
+
+        // 다른 사각형을 만들기 위해서는 코드가 중복됨
+        int width2 = 10;
+        int height2 = 20;
+
+        int area2 = width2 * height2;
+    }
+}
