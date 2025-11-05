f0e280a5fe47403ea9d57602438aa73fb8125d89
yu-rim98 <rimmm098@gmail.com>
2025-11-05 13:42:34 +0900

    Person 클래스 정의

---
+++ week07/java/src/b/oop/Person.java
@@ -0,0 +1,17 @@
+package b.oop;
+
+public class Person {
+
+    // 데이터, 필드, 속성
+    String name;
+    int age;
+
+    // 생성자
+    public Person() {}
+
+    // 메서드, 행동
+    void introduce() {
+        System.out.println("Hello");
+    }
+
+}
