f9bb2ff498c677766d4bf2c028f384ed4f887850
yu-rim98 <rimmm098@gmail.com>
2025-11-05 09:15:53 +0900

    배열 - Arrays 메서드 활용(정렬, 검색)

---
+++ week07/java/src/a/basic/ArraysUtility.java
@@ -0,0 +1,21 @@
+package a.basic;
+
+import java.util.Arrays;
+
+public class ArraysUtility {
+
+    public static void main(String[] args) {
+        // Array 유틸리티 클래스
+        int[] numbers = {5, 2, 1, 7, 8};
+
+        System.out.println(Arrays.toString(numbers));
+
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        // binarySearchs는 배열이 정렬되어 있어야 값을 찾을 수 있음
+        int index = Arrays.binarySearch(numbers, 7);
+        System.out.println(index);
+
+    }
+}
