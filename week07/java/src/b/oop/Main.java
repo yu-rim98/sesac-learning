1b1b6ee36d933713d0490b2323d6314385937373
yu-rim98 <rimmm098@gmail.com>
2025-11-05 14:02:53 +0900

    클래스 생성 - 클래스를 통해 사각형 객체 여러개 만듦

---
+++ week07/java/src/b/oop/Main.java
@@ -0,0 +1,14 @@
+package b.oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(10, 20);
+        Circle c1 = new Circle(10);
+
+        Rectangle r2 = new Rectangle(20, 20);
+        Rectangle r3 = new Rectangle(100, 100);
+
+    }
+
+}
