ded318b61b9ab9cfa65618d92fa3544f44d137e0
yu-rim98 <rimmm098@gmail.com>
2025-11-06 09:40:24 +0900

    생성자 - 메서드 오버로딩

--- week07/java/src/c/oop2/Main.java
+++ week07/java/src/c/oop2/Main.java
@@ -6,5 +6,8 @@ public class Main {
         Calculator calculator = new Calculator();
 
         calculator.printResult(calculator.add(1, 2));
+
+        Person p1 = new Person();
+        Person p2 = new Person("kim", 20);
     }
 }
---
+++ week07/java/src/c/oop2/Person.java
@@ -0,0 +1,17 @@
+package c.oop2;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person() {
+        this.name = "noname";
+        this.age = 0;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
