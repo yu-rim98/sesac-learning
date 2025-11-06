3b6f855538a4df9b21587d2ea0c53e8e4bb96f71
yu-rim98 <rimmm098@gmail.com>
2025-11-06 09:20:04 +0900

    Calculator 메서드 사용

---
+++ week07/java/src/c/oop2/Calculator.java
@@ -0,0 +1,13 @@
+package c.oop2;
+
+public class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    void printResult(int result) {
+        System.out.println(result);
+    }
+
+}
---
+++ week07/java/src/c/oop2/Main.java
@@ -0,0 +1,10 @@
+package c.oop2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        calculator.printResult(calculator.add(1, 2));
+    }
+}
