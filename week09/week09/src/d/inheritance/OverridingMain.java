502e7fdae4c1b72d11544468559c1f20cd65e1f7
yu-rim98 <rimmm098@gmail.com>
2025-11-10 11:02:47 +0900

    상속 - final 메서드는 오버라이딩 불가함

--- week09/week09/src/d/inheritance/OverridingMain.java
+++ week09/week09/src/d/inheritance/OverridingMain.java
@@ -4,6 +4,10 @@ class Parent {
     void show() {
         System.out.println("부모 클래스");
     }
+
+    final void display() {
+        System.out.println("표시");
+    }
 }
 
 class Child extends Parent {
@@ -11,6 +15,11 @@ class Child extends Parent {
     @Override
     void show() {
         System.out.println("자식 클래스");
+    }
+
+    @Override
+    void display() {
+        System.out.println("자식이 표시");
     }
 }
 
