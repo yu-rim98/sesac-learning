dde7618063a5ba91ee8648a3808c31f1fbc4bf1f
yu-rim98 <rimmm098@gmail.com>
2025-11-10 10:34:41 +0900

    상속 - 부모 클래스의 필드 접근

--- week09/week09/src/d/inheritance/SuperMain.java
+++ week09/week09/src/d/inheritance/SuperMain.java
@@ -4,10 +4,15 @@ class Animal2 {
 
     String name;
     int age;
+    String bread;
 
     Animal2(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    void introduce() {
+        System.out.println("hello " + this.name + this.age);
     }
 }
 
@@ -19,12 +24,30 @@ class Dog2 extends Animal2 {
         this.bread = bread;
     }
 
+    void display() {
+        System.out.println(this.bread);
+        System.out.println(super.bread);
+    }
+
+    void introduce() {
+        super.introduce();
+        System.out.println("멍멍");
+    }
 }
 
 public class SuperMain {
 
     public static void main(String[] args) {
         Animal2 animal2 = new Animal2("동물", 0);
+        Dog2 dog2 = new Dog2("초코", 3, "말티즈");
+
+        System.out.println(animal2.bread);
+        System.out.println(dog2.bread);
+
+        dog2.display();
+        dog2.introduce();
+        animal2.introduce();
+
     }
 
 }
