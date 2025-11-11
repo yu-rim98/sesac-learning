0ed432a03c05dc9d58691f567692019cbed6b26e
yu-rim98 <rimmm098@gmail.com>
2025-11-11 09:43:08 +0900

    class 앞에 abstract 키워드를 사용해 추상클래스를 정의할 수 있다. 추상메서드는 구현 없이 정의만 하는 것 -> 추상 클래스를 상속받는 자식 클래스에서 무조건 구현해야 한다.

--- week09/week09/src/d/inheritance/AbstractMain.java
+++ week09/week09/src/d/inheritance/AbstractMain.java
@@ -6,11 +6,19 @@ abstract class Animal4 {
     public Animal4(String name) {
         this.name = name;
     }
+
+    void sleep() {
+        System.out.println("잠자기");
+    }
+
+    // 추상메서드 - 추상클래스를 상속받는 자식이 무조건 구현해야 함
+    abstract void makeSound();
 }
+
 public class AbstractMain {
 
     public static void main(String[] args) {
-        Animal4 a = new Animal4("동물");
+        // Animal4 a = new Animal4("동물");
     }
 
 }
