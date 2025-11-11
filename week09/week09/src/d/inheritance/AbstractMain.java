5fd0cbb7ac0a00068aae3e1563e5d871f062acd6
yu-rim98 <rimmm098@gmail.com>
2025-11-11 09:37:37 +0900

    추상클래스 - 일반 클래스는 인스턴스를 생성할 수 있다.

---
+++ week09/week09/src/d/inheritance/AbstractMain.java
@@ -0,0 +1,16 @@
+package d.inheritance;
+
+class Animal4 {
+    String name;
+
+    public Animal4(String name) {
+        this.name = name;
+    }
+}
+public class AbstractMain {
+
+    public static void main(String[] args) {
+        Animal4 a = new Animal4("동물");
+    }
+
+}
