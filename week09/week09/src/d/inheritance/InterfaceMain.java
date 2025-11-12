64662c5532095ad6e4a1598c93ba111f412abf2e
yu-rim98 <rimmm098@gmail.com>
2025-11-12 09:18:43 +0900

    인터페이스

---
+++ week09/week09/src/d/inheritance/InterfaceMain.java
@@ -0,0 +1,52 @@
+package d.inheritance;
+
+abstract class Animal5 {
+    abstract void run();
+    abstract void sleep();
+    abstract void eat();
+}
+
+class Dog5 extends Animal5 {
+
+    @Override
+    void run() {
+
+    }
+
+    @Override
+    void sleep() {
+
+    }
+
+    @Override
+    void eat() {
+
+    }
+}
+
+interface Animal6 {
+    void run();
+    void sleep();
+    void eat();
+}
+
+class Dog6 implements Animal6 {
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
+public class InterfaceMain {
+
+}
