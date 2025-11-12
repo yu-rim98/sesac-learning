6e00a1a0d3fc66e46b903a699d0045becf6c142f
yu-rim98 <rimmm098@gmail.com>
2025-11-12 13:42:05 +0900

    제네릭 제약

--- week09/week09/src/e/generic/ExtendsGeneric.java
+++ week09/week09/src/e/generic/ExtendsGeneric.java
@@ -11,6 +11,9 @@ class NumberBox<T extends Number> { // Number 아래 타입만 받도록 지정
         return value;
     }
 
+    public double getDoubleValue() {
+        return (double) value;
+    }
 }
 
 public class ExtendsGeneric {
@@ -18,6 +21,7 @@ public class ExtendsGeneric {
     public static void main(String[] args) {
         NumberBox<Integer> numberBox1 = new NumberBox<>(10);
         int i = numberBox1.getValue();
+        double d1 = numberBox1.getDoubleValue();
 
         NumberBox<Double> numberBox2 = new NumberBox<>(3.14);
         double d = numberBox2.getValue();
