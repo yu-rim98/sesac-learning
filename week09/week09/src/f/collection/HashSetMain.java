3b5ad962510f5a627e22b510ada63716e3e08639
yu-rim98 <rimmm098@gmail.com>
2025-11-13 13:34:48 +0900

    Set - 집합 연산

--- week09/week09/src/f/collection/HashSetMain.java
+++ week09/week09/src/f/collection/HashSetMain.java
@@ -1,6 +1,8 @@
 package f.collection;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class HashSetMain {
 
@@ -32,6 +34,24 @@ public class HashSetMain {
             System.out.println(item);
         }
 
+        Set<Integer> intSet1 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5));
+        Set<Integer> intSet2 = new HashSet<>(Arrays.asList(4, 5, 6, 7, 8));
+
+        // 합집합 (Union)
+        intSet1.addAll(intSet2);
+        System.out.println(intSet1);
+
+        // 교집합 (Intersection)
+        intSet1.retainAll(intSet2);
+        System.out.println(intSet1);
+
+        // 차집합 (Difference)
+        intSet1.removeAll(intSet2);
+        System.out.println(intSet1);
+
+        // 부분집합 확인
+        boolean result = intSet1.containsAll(intSet2);
+        System.out.println(result);
 
     }
 }
