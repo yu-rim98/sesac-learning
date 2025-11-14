a4936d7e39f342a6df37750c70b52f133e55e7ab
yu-rim98 <rimmm098@gmail.com>
2025-11-14 10:14:56 +0900

    예외처리4 - 사용자 정의 예외

---
+++ week09/week09/src/h/exception/BankAccount.java
@@ -0,0 +1,42 @@
+package h.exception;
+
+
+// 보통 Exception을 상속받지 않고 런타임 Exception을 상속받는 이유는 무엇인가?
+class InsufficientBalanceException extends Exception {
+
+    private int current;
+    private int request;
+
+    public InsufficientBalanceException(int current, int request) {
+        super("잔액 부족 " + current + " / " + request);
+        this.current = current;
+        this.request = request;
+    }
+}
+
+public class BankAccount {
+
+    private int balance;
+
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(int amount) throws InsufficientBalanceException {
+        if (amount > balance) {
+            throw new InsufficientBalanceException(balance, amount);
+        }
+        balance -= amount;
+    }
+
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount(10000);
+        try {
+            ba.withdraw(100000);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(ba.balance);
+    }
+}
