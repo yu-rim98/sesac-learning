a290ce362a7e2b5005bf30a743de79c08fc0a6bf
yu-rim98 <rimmm098@gmail.com>
2025-11-14 11:08:10 +0900

    람다식

---
+++ week09/week09/src/i/lambda/LambdaMain.java
@@ -0,0 +1,52 @@
+package i.lambda;
+
+import java.util.Comparator;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+interface Calculator {
+    int calculate(int a, int b);
+
+}
+
+
+public class LambdaMain {
+
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(a, b);
+            }
+        };
+
+        // 알아서 클래스로 만들어줌
+        // 즉, 클래스를 인스턴스화 하고 메서드로 만들어줌
+        Comparator<Integer> comparator2 = (a, b) -> Integer.compare(a, b);
+        
+        // 매개변수가 없는 형태
+        Runnable task = () -> System.out.println("실행");
+        task.run();
+
+        // 매개변수 하나
+        Consumer<String> print = (s) -> System.out.println(s);
+        print.accept("하이");
+
+        // 매개변수 2개
+        BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
+        int result = add.apply(2, 2);
+        System.out.println(result);
+
+        BiFunction<Integer, Integer, Integer> calc = (a, b) -> {
+            int sum = 1 + b;
+            return sum / 2;
+        };
+
+        System.out.println(calc.apply(5, 10));
+
+
+        // 그럼 람다는 인터페이스에만 사용할 수 있나 ?
+        Calculator cal = (a, b) -> a + b;
+
+     }
+}
