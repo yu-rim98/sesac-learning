766fbed166fe3abe8a67bde2afe8f56e9a741209
yu-rim98 <rimmm098@gmail.com>
2025-11-17 11:36:51 +0900

    model 실습
    model에 데이터를 담으면 타임리프에서 꺼내 사용할 수 있음

--- week10/firstapp/src/main/java/org/example/firstapp/controller/HomeController.java
+++ week10/firstapp/src/main/java/org/example/firstapp/controller/HomeController.java
@@ -1,5 +1,7 @@
 package org.example.firstapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,4 +21,13 @@ public class HomeController {
 
         return "hello";
     }
+
+    @GetMapping("/user")
+    public String user(Model model) {
+        model.addAttribute("username", "kim");
+        model.addAttribute("age", 20);
+        model.addAttribute("city", "seoul");
+        return "user";
+    }
+
 }
