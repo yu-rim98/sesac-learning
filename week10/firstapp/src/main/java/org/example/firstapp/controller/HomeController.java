7d9b4628719e5ba1193f2e26d7e143fdb109a94a
yu-rim98 <rimmm098@gmail.com>
2025-11-17 11:21:16 +0900

    타임리프 실습

--- week10/firstapp/src/main/java/org/example/firstapp/controller/HomeController.java
+++ week10/firstapp/src/main/java/org/example/firstapp/controller/HomeController.java
@@ -1,6 +1,7 @@
 package org.example.firstapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,13 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        String name = "yurim";
+        model.addAttribute("name", name);
+
+        return "hello";
     }
 }
