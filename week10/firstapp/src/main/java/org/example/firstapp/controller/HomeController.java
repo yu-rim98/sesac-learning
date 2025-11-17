8c2e2b1c4b23f4bd016d93e3038c2fbd45ea9c5a
yu-rim98 <rimmm098@gmail.com>
2025-11-17 14:01:17 +0900

    model 실습5 - PathVariable

--- week10/firstapp/src/main/java/org/example/firstapp/controller/HomeController.java
+++ week10/firstapp/src/main/java/org/example/firstapp/controller/HomeController.java
@@ -10,6 +10,7 @@ import java.util.stream.IntStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -77,4 +78,12 @@ public class HomeController {
 
         return "lotto";
     }
+
+
+    @GetMapping("/profile/{username}")
+    public String profile(@PathVariable String username, Model model) {
+        model.addAttribute("username", username);
+        return "profile";
+    }
+
 }
