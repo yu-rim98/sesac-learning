6a739ee322e647e30e9155ca4c7852a1a0e9ceb5
yu-rim98 <rimmm098@gmail.com>
2025-11-18 11:42:42 +0900

    샘플 데이터 init

--- week10/todoapp/src/main/java/org/example/todoapp/TodoappApplication.java
+++ week10/todoapp/src/main/java/org/example/todoapp/TodoappApplication.java
@@ -1,7 +1,11 @@
 package org.example.todoapp;
 
+import org.example.todoapp.dto.TodoDto;
+import org.example.todoapp.repository.TodoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoappApplication {
@@ -10,4 +14,12 @@ public class TodoappApplication {
         SpringApplication.run(TodoappApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner init(TodoRepository todoRepository) {
+        return args -> {
+            todoRepository.save(new TodoDto(null, "드라마 보기", "갯마을 차차차", false));
+            todoRepository.save(new TodoDto(null, "영화 보기", "이터널선샤인 ", false));
+            todoRepository.save(new TodoDto(null, "운동하기", "헬스", false));
+        };
+    }
 }
