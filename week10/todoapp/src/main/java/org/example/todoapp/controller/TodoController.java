f034e968811ad92c799081f9b7dd3f2270a25a05
yu-rim98 <rimmm098@gmail.com>
2025-11-18 10:35:04 +0900

    Todo 생성

--- week10/todoapp/src/main/java/org/example/todoapp/controller/TodoController.java
+++ week10/todoapp/src/main/java/org/example/todoapp/controller/TodoController.java
@@ -1,7 +1,11 @@
 package org.example.todoapp.controller;
 
+import org.example.todoapp.dto.TodoDto;
+import org.example.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -9,6 +13,23 @@ public class TodoController {
     @GetMapping("/todos")
     public String todos() {
         return "todos";
+    }
+
+    @GetMapping("/todos/new")
+    public String newTodo() {
+        return "new";
+    }
+
+    @GetMapping("/todos/create")
+    public String create(@RequestParam String title, @RequestParam String content,
+        Model model) {
+        TodoDto todoDto = new TodoDto(null, title, content, false);
+        TodoRepository todoRepository = new TodoRepository();
+        TodoDto saveTodo = todoRepository.save(todoDto);
+
+        model.addAttribute("todo", saveTodo);
+
+        return "create";
     }
 
 }
--- week10/todoapp/src/main/java/org/example/todoapp/repository/TodoRepository.java
+++ week10/todoapp/src/main/java/org/example/todoapp/repository/TodoRepository.java
@@ -9,7 +9,12 @@ public class TodoRepository {
     private final Map<Long, TodoDto> storage = new ConcurrentHashMap<>();
     private Long nextId = 1L;
 
-
+    
+    public TodoDto save(TodoDto todo) {
+        todo.setId(nextId++);
+        storage.put(todo.getId(), todo);
+        return todo;
+    }
 
 
 }
