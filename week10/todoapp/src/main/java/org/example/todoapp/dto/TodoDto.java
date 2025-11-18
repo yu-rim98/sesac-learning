93c9b368a475f976e1950029d6e6ba151fc17e83
yu-rim98 <rimmm098@gmail.com>
2025-11-18 09:30:25 +0900

    TodoDto 설정

---
+++ week10/todoapp/src/main/java/org/example/todoapp/dto/TodoDto.java
@@ -0,0 +1,48 @@
+package org.example.todoapp.dto;
+
+public class TodoDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private boolean completed;
+
+    public TodoDto(Long id, String title, String content, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
