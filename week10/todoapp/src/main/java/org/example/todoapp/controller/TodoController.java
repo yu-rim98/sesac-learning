d76ff04f8a47f532554b2a978614eb75780d933d
yu-rim98 <rimmm098@gmail.com>
2025-11-19 13:34:10 +0900

    토글 기능 구현

--- week10/todoapp/src/main/java/org/example/todoapp/controller/TodoController.java
+++ week10/todoapp/src/main/java/org/example/todoapp/controller/TodoController.java
@@ -129,4 +129,20 @@ public class TodoController {
         return "todos";
     }
 
+    @GetMapping("/todos/{id}/toggle")
+    public String toggle(@PathVariable Long id, Model model) {
+        try {
+            TodoDto todo = todoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 todo입니다."));
+            todo.setCompleted(!todo.isCompleted());
+
+            todoRepository.save(todo);
+
+            return "redirect:/todos/" + id;
+
+        } catch (IllegalArgumentException e) {
+            return "redirect:/todos";
+        }
+    }
+
 }
