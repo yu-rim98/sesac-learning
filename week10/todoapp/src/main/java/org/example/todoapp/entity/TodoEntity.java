faf5f364d72db973104d37fce7c773044a247a85
yu-rim98 <rimmm098@gmail.com>
2025-11-20 15:07:23 +0900

    엔티티 정의

---
+++ week10/todoapp/src/main/java/org/example/todoapp/entity/TodoEntity.java
@@ -0,0 +1,52 @@
+package org.example.todoapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private boolean completed;
+
+    public TodoEntity() {
+    }
+
+    public TodoEntity(Long id, String title, String content, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.completed = completed;
+    }
+
+    public TodoEntity(String title, String content, boolean completed) {
+        this.title = title;
+        this.content = content;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
