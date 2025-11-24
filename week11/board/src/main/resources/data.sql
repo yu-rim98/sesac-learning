1d4b2f4d2c7191dbec7f0aba2099229ed6ba46e3
yu-rim98 <rimmm098@gmail.com>
2025-11-24 09:26:54 +0900

    테이블, 데이터 생성 sql 파일

---
+++ week11/board/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO post (title, content) VALUES ('첫번째 게시물', 'Hello');
+INSERT INTO post (title, content) VALUES ('두번째 게시물', 'Hi');
+INSERT INTO post (title, content) VALUES ('세번째 게시물', 'Bye');
