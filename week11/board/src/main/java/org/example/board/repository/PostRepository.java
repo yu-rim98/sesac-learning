273fd6bdf6e3933e9b1561f22a447d887da0dc75
yu-rim98 <rimmm098@gmail.com>
2025-11-24 14:24:27 +0900

    JDBC -> JPA로 변경

--- week11/board/src/main/java/org/example/board/repository/PostRepository.java
+++ week11/board/src/main/java/org/example/board/repository/PostRepository.java
@@ -1,57 +1,38 @@
 package org.example.board.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.example.board.dto.PostDto;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.example.board.entity.Post;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
 public class PostRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    @PersistenceContext
+    private EntityManager em;
 
-    // row 한줄을 dto로 바꿔줌
-    // DB 조회 결과를 DTO 객체로 변환해주는 역할
-    private final RowMapper<PostDto> rowMapper = (rs, rowNum) -> {
-        return new PostDto(
-            // 데이터 타입과 컬럼명을 명시해서 가져옴
-            rs.getLong("id"),
-            rs.getString("title"),
-            rs.getString("content"),
-            rs.getTimestamp("created_at").toLocalDateTime()
-        );
-    };
-
-    // 전체 조회
-    public List<PostDto> findAll() {
-        String sql = "select * from post";
-
-        return jdbcTemplate.query(sql, rowMapper);
-    }
-
-    // 상세 조회
-    public PostDto findById(Long id) {
-        String sql = "select * from post where id = ?";
-        // queryForObject => 단일 행 조회
-        PostDto post = jdbcTemplate.queryForObject(sql, rowMapper, id); // id = ?에 들어갈 값
+    public Post save(Post post) {
+        em.persist(post);
         return post;
     }
 
-    public void save(PostDto postDto) {
-        String sql = "insert into post (title, content) values (?, ?)";
-        jdbcTemplate.update(sql, postDto.getTitle(), postDto.getContent());
+    public Post findById(Long id) {
+        return em.find(Post.class, id); // 엔티티의 정보와 id를 전달
     }
 
-    public void update(Long id, PostDto postDto) {
-        String sql = "update post set title = ?, content = ? where id = ?";
-        jdbcTemplate.update(sql, postDto.getTitle(), postDto.getContent(), id);
+    public List<Post> findAll() {
+        // EM -> 단일 엔티티 조작만 기본으로 제공됨
+        String jpql = "select p from Post p";
+        return em.createQuery(jpql, Post.class).getResultList();
     }
 
-    public void deleteById(Long id) {
-        String sql = "delete from post where id = ?";
-        jdbcTemplate.update(sql, id);
+    public Post update(Post post) {
+        return em.merge(post);
     }
+
+    public void delete(Post post) {
+        em.remove(post);
+    }
+
 }
