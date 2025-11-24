f9746fab11f858ef981749c39539e674664ae7f3
yu-rim98 <rimmm098@gmail.com>
2025-11-24 11:43:59 +0900

    jpa, entity 설정

---
+++ week11/board/src/main/java/org/example/board/entity/Post.java
@@ -0,0 +1,42 @@
+package org.example.board.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // 생성 시점에 자동으로 현재 시간을 설정
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
