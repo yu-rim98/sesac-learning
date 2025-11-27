916d93f7977ad3f13356b8bd2aee4bd1b70550ae
yu-rim98 <rimmm098@gmail.com>
2025-11-27 09:21:21 +0900

    either 프로젝트 init

---
+++ week11/either/src/main/java/com/example/either/EitherApplication.java
@@ -0,0 +1,13 @@
+package com.example.either;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EitherApplication.class, args);
+	}
+
+}
---
+++ week11/either/src/test/java/com/example/either/EitherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.either;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EitherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
