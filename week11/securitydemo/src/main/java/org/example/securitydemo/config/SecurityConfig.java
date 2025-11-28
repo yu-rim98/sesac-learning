7fbcb9ad3bae0aae4e31cfb2f4eeea5c2ddc8ca3
yu-rim98 <rimmm098@gmail.com>
2025-11-28 14:40:37 +0900

    Inmemory 제거 / DB 설정

--- week11/securitydemo/src/main/java/org/example/securitydemo/config/SecurityConfig.java
+++ week11/securitydemo/src/main/java/org/example/securitydemo/config/SecurityConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,7 +17,7 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(
                 auth -> auth
-                    .requestMatchers("/", "/info", "/login").permitAll() // 허용
+                    .requestMatchers("/", "/info", "/login", "/h2-console/**").permitAll() // 허용
                     .requestMatchers("/admin/**").hasRole("ADMIN") // ADMIN 권한만 접근 가능
                     .requestMatchers("/dashboard/**")
                     .hasAnyRole("USER", "ADMIN") // USER, ADMIN 접근 가능
@@ -37,27 +35,34 @@ public class SecurityConfig {
             )
             .exceptionHandling(ex -> ex
                 .accessDeniedPage("/access-denied")
-            )
-        ;
+            );
+
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));
+        http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-            .username("user")
-            .password("{noop}1234")
-            .roles("USER")
-            .build();
-
-        UserDetails admin = User.builder()
-            .username("admin")
-            .password("{noop}admin")
-            .roles("USER", "ADMIN")
-            .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.builder()
+//            .username("user")
+//            .password("{noop}1234")
+//            .roles("USER")
+//            .build();
+//
+//        UserDetails admin = User.builder()
+//            .username("admin")
+//            .password("{noop}admin")
+//            .roles("USER", "ADMIN")
+//            .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 
 }
