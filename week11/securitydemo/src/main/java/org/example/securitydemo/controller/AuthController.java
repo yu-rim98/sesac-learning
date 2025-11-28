d44beae50e7cf9d4f4cc58b6936c8a50075077f4
yu-rim98 <rimmm098@gmail.com>
2025-11-28 11:14:26 +0900

    로그인한 사용자 정보 출력

--- week11/securitydemo/src/main/java/org/example/securitydemo/controller/AuthController.java
+++ week11/securitydemo/src/main/java/org/example/securitydemo/controller/AuthController.java
@@ -1,6 +1,10 @@
 package org.example.securitydemo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +16,14 @@ public class AuthController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+
+        if (userDetails != null) {
+            model.addAttribute("name", userDetails.getUsername());
+            model.addAttribute("authorities", userDetails.getAuthorities());
+            model.addAttribute("password", userDetails.getPassword());
+        }
+
         return "dashboard";
     }
 }
