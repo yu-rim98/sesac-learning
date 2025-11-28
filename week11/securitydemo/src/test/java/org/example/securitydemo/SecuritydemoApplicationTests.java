a33467aa9c25cb9876a2f1ba1f9cf900fe056bc1
yu-rim98 <rimmm098@gmail.com>
2025-11-28 09:07:10 +0900

    security project init

---
+++ week11/securitydemo/src/test/java/org/example/securitydemo/SecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
