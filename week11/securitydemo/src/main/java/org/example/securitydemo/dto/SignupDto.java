f568725bb55609ffab46c1dbde5400dfde19a6ed
yu-rim98 <rimmm098@gmail.com>
2025-12-01 09:24:12 +0900

    SignupDto 추가

---
+++ week11/securitydemo/src/main/java/org/example/securitydemo/dto/SignupDto.java
@@ -0,0 +1,27 @@
+package org.example.securitydemo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+//@Setter
+public class SignupDto {
+
+    @NotBlank(message = "사용자명을 입력해주세요.")
+    @Size(min = 3, max = 20, message = "사용자명은 3~20자입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 4, max = 20, message = "비밀번호는 4~20자입니다.")
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인을 입력해주세요.")
+    private String passwordConfirm;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+}
