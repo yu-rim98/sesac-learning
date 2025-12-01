f9778c5d2e37032cfe93e1797a001a8bb56c7942
yu-rim98 <rimmm098@gmail.com>
2025-12-01 14:54:20 +0900

    Refactor: abstract 명시

--- week11/instagram/src/main/java/org/example/instagram/entity/BaseEntity.java
+++ week11/instagram/src/main/java/org/example/instagram/entity/BaseEntity.java
@@ -12,9 +12,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass // 엔티티 간의 공통된 매핑 정보 상속을 위한 클래스임을 명시하는 어노테이션
 @EntityListeners(AuditingEntityListener.class) // JPA 엔티티 생명주기 이벤트를 감지
-public class BaseEntity {
+public abstract class BaseEntity {
 
-    @Column(nullable = false)
+    @Column(updatable = false)
     @CreatedDate
     private LocalDateTime createdAt;
 
