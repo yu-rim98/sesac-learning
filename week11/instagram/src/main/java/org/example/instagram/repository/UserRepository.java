219028f4d2a88cebfd641dce00c006d12eceb7a0
yu-rim98 <rimmm098@gmail.com>
2025-12-01 15:40:36 +0900

    Feat: 회원가입 시 username 중복 검증 추가

--- week11/instagram/src/main/java/org/example/instagram/repository/UserRepository.java
+++ week11/instagram/src/main/java/org/example/instagram/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package org.example.instagram.repository;
 
+import java.util.Optional;
 import org.example.instagram.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
