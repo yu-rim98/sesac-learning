bec323d0b31ce3ac0c9cfd765c9d4f1b4f975862
yu-rim98 <rimmm098@gmail.com>
2025-12-01 10:17:11 +0900

    세터, 기본 생성자 어노테이션 추가

--- week11/securitydemo/src/main/java/org/example/securitydemo/dto/SignupDto.java
+++ week11/securitydemo/src/main/java/org/example/securitydemo/dto/SignupDto.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-//@Setter
+@Setter
+@NoArgsConstructor
 public class SignupDto {
 
     @NotBlank(message = "사용자명을 입력해주세요.")
