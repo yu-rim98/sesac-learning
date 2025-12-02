5b4f8a38950649798c3fde11e37a4076a8962ca7
yu-rim98 <rimmm098@gmail.com>
2025-12-02 10:15:56 +0900

    Feat: Post 엔티티 추가

---
+++ week11/instagram/src/main/java/org/example/instagram/entity/Post.java
@@ -0,0 +1,39 @@
+package org.example.instagram.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "posts")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Post(String content, User user) {
+        this.content = content;
+        this.user = user;
+    }
+
+}
