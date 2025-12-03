6621c528d186e03c3b9baf6330fccc75d53c0003
yu-rim98 <rimmm098@gmail.com>
2025-12-03 14:47:43 +0900

    Feat: Follow 엔티티 정의

---
+++ week11/instagram/src/main/java/org/example/instagram/entity/Follow.java
@@ -0,0 +1,38 @@
+package org.example.instagram.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "follows")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id", nullable = false)
+    private User following;
+
+    @Builder
+    public Follow(User follower, User following) {
+        this.follower = follower;
+        this.following = following;
+    }
+}
