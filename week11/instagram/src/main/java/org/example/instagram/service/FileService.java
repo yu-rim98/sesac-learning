1302ee39cb88ff07a794f4248aafe749469865f8
yu-rim98 <rimmm098@gmail.com>
2025-12-04 14:04:41 +0900

    Feat: 파일 저장 로직 추가

---
+++ week11/instagram/src/main/java/org/example/instagram/service/FileService.java
@@ -0,0 +1,8 @@
+package org.example.instagram.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+
+    String saveFile(MultipartFile file);
+}
---
+++ week11/instagram/src/main/java/org/example/instagram/service/FileServiceImpl.java
@@ -0,0 +1,67 @@
+package org.example.instagram.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    private static final List<String> ALLOWED_EXTENSIONS = Arrays.asList(".jpg", ".jpeg", ".png",
+        ".gif");
+
+    @Override
+    public String saveFile(MultipartFile file) {
+        try {
+            if (file == null || file.isEmpty()) {
+                return null;
+            }
+
+            String extension = getExtension(file.getOriginalFilename());
+            validationExtension(extension);
+
+            Path uploadPath = Paths.get(uploadDir);
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            String savedFileName = getSavedFileName(extension);
+
+            Path filePath = uploadPath.resolve(savedFileName);
+            Files.copy(file.getInputStream(), filePath);
+
+            return savedFileName;
+
+        } catch (IOException e) {
+            throw new RuntimeException("파일 저장 중 오류");
+        }
+    }
+
+    private String getExtension(String filename) {
+        if (filename == null || !filename.contains(".")) {
+            throw new RuntimeException("유효하지 않은 파일");
+        }
+
+        return filename.substring(filename.lastIndexOf("."));
+    }
+
+    private void validationExtension(String extension) {
+        if (!ALLOWED_EXTENSIONS.contains(extension.toLowerCase())) {
+            throw new RuntimeException("허용되지 않는 확장자");
+        }
+    }
+
+    private String getSavedFileName(String extension) {
+        return UUID.randomUUID() + extension;
+    }
+}
