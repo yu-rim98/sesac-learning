ee44fabfaf16e5e3f1e276dd2eaaf0b063d94ff6
yu-rim98 <rimmm098@gmail.com>
2025-12-04 11:22:42 +0900

    Feat: EntityGraph 적용

--- week11/instagram/src/main/java/org/example/instagram/controller/HomeController.java
+++ week11/instagram/src/main/java/org/example/instagram/controller/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("posts", postService.getAllPosts());
+        model.addAttribute("posts", postService.getAllPostsWithStats());
         return "home";
     }
 }
