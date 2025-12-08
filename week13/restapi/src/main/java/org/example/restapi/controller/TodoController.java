67605b499f4646d5738682ee09ad59884e8c87f6
yu-rim98 <rimmm098@gmail.com>
2025-12-08 14:37:13 +0900

    Refactor: ApiResponse 적용

--- week13/restapi/src/main/java/org/example/restapi/controller/TodoController.java
+++ week13/restapi/src/main/java/org/example/restapi/controller/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.restapi.dto.request.TodoCreateReq;
 import org.example.restapi.dto.request.TodoUpdateReq;
+import org.example.restapi.dto.response.ApiResponse;
 import org.example.restapi.dto.response.TodoResponse;
 import org.example.restapi.service.TodoService;
 import org.springframework.http.HttpStatus;
@@ -26,29 +27,35 @@ public class TodoController {
     private final TodoService todoService;
 
     @PostMapping
-    public ResponseEntity<TodoResponse> createTodo(@Valid @RequestBody TodoCreateReq req) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(todoService.create(req));
+    public ResponseEntity<ApiResponse<TodoResponse>> createTodo(
+        @Valid @RequestBody TodoCreateReq req) {
+        TodoResponse todoResponse = todoService.create(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(todoResponse));
     }
 
     @GetMapping
-    public ResponseEntity<List<TodoResponse>> getAll() {
-        return ResponseEntity.ok(todoService.findAll());
+    public ResponseEntity<ApiResponse<List<TodoResponse>>> getAll() {
+        List<TodoResponse> responses = todoService.findAll();
+        return ResponseEntity.ok(ApiResponse.success(responses));
     }
 
     @GetMapping("/{todoId}")
-    public ResponseEntity<TodoResponse> findById(@PathVariable Long todoId) {
-        return ResponseEntity.ok(todoService.findById(todoId));
+    public ResponseEntity<ApiResponse<TodoResponse>> findById(@PathVariable Long todoId) {
+        TodoResponse todoResponse = todoService.findById(todoId);
+
+        return ResponseEntity.ok(ApiResponse.success(todoResponse));
     }
 
     @DeleteMapping("/{todoId}")
-    public ResponseEntity<String> deleteById(@PathVariable Long todoId) {
+    public ResponseEntity<ApiResponse<Void>> deleteById(@PathVariable Long todoId) {
         todoService.deleteById(todoId);
-        return ResponseEntity.ok("삭제되었습니다.");
+        return ResponseEntity.ok(ApiResponse.success());
     }
 
     @PutMapping("/{todoId}")
-    public ResponseEntity<TodoResponse> update(@PathVariable Long todoId,
+    public ResponseEntity<ApiResponse<TodoResponse>> update(@PathVariable Long todoId,
         @Valid @RequestBody TodoUpdateReq req) {
-        return ResponseEntity.ok(todoService.update(todoId, req));
+        TodoResponse updateTodo = todoService.update(todoId, req);
+        return ResponseEntity.ok(ApiResponse.success(updateTodo));
     }
 }
