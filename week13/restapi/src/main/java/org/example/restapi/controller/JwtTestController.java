ec34159fa32e943b5fb6c03c1c3215131c5bc6e3
yu-rim98 <rimmm098@gmail.com>
2025-12-09 10:42:02 +0900

    Feat: JWT 토큰 검증 테스트

--- week13/restapi/src/main/java/org/example/restapi/controller/JwtTestController.java
+++ week13/restapi/src/main/java/org/example/restapi/controller/JwtTestController.java
@@ -18,10 +18,21 @@ public class JwtTestController {
     private final JwtProvider jwtProvider;
 
     @GetMapping("/generate")
-    public ResponseEntity<ApiResponse<Map<String, String>>> generateToken(@RequestParam String username) {
+    public ResponseEntity<ApiResponse<Map<String, String>>> generateToken(
+        @RequestParam String username) {
         String token = jwtProvider.createToken(username);
         return ResponseEntity.ok(ApiResponse.success(Map.of("token", token)));
     }
 
-//    @GetMapping("/validate")
+    @GetMapping("/validate")
+    public ResponseEntity<ApiResponse<Map<String, Object>>> validateToken(
+        @RequestParam String token) {
+        boolean valid = jwtProvider.validateToken(token);
+        String username = valid ? jwtProvider.getUsername(token) : null;
+
+        return ResponseEntity.ok(ApiResponse.success(
+            Map.of("valid", valid,
+                "username", username)
+        ));
+    }
 }
