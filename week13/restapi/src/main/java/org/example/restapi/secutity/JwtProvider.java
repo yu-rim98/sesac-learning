57b9642fcfc4ba787ea2f240ba21f634c387dd0d
yu-rim98 <rimmm098@gmail.com>
2025-12-09 10:15:45 +0900

    Feat: JWTProvider 작성

---
+++ week13/restapi/src/main/java/org/example/restapi/secutity/JwtProvider.java
@@ -0,0 +1,77 @@
+package org.example.restapi.secutity;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secretString;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(secretString.getBytes());
+    }
+
+    // JWT 토큰 발행
+    private String createToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+            .subject(username)
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(secretKey)
+            .compact();
+    }
+
+    // claims 검증
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    // 토큰에서 사용자명 추출
+    public String getUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    // 토큰 유효성 검사
+    public boolean validateToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            System.out.println("만료된 JWT입니다.");
+        } catch (UnsupportedJwtException e) {
+            System.out.println("지원하지 않는 JWT입니다.");
+        } catch (MalformedJwtException e) {
+            System.out.println("잘못된 JWT입니다.");
+        } catch (SignatureException e) {
+            System.out.println("잘못된 서명입니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("토큰이 없습니다.");
+        }
+        return false;
+    }
+}
