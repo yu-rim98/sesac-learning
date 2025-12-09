e6dddf79c99112640a4d12cbf06d4429f705eef7
yu-rim98 <rimmm098@gmail.com>
2025-12-09 11:19:29 +0900

    Feat: 에러 추가

--- week13/restapi/src/main/java/org/example/restapi/exception/ErrorCode.java
+++ week13/restapi/src/main/java/org/example/restapi/exception/ErrorCode.java
@@ -12,7 +12,10 @@ public enum ErrorCode {
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "서버 오류가 발생했습니다."),
 
     // todo 에러
-    TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "TODO_NOT_FOUND", "존재하지 않는 Todo입니다.");
+    TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "TODO_NOT_FOUND", "존재하지 않는 Todo입니다."),
+
+    // user 에러
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "존재하지 않는 사용자입니다.");
 
     private final HttpStatus status;
     private final String code;
