04184457e3e60eb117dcf3d60b473d80399b951b
yu-rim98 <rimmm098@gmail.com>
2025-12-09 11:19:06 +0900

    Feat: CustomUserDetails 구현

---
+++ week13/restapi/src/main/java/org/example/restapi/secutity/CustomUserDetails.java
@@ -0,0 +1,30 @@
+package org.example.restapi.secutity;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.restapi.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
