f8f507368638ff94e14e935e5807e470f878817c
yu-rim98 <rimmm098@gmail.com>
2025-12-09 10:51:07 +0900

    Feat: User 엔티티 정의

---
+++ week13/restapi/src/main/java/org/example/restapi/entity/User.java
@@ -0,0 +1,49 @@
+package org.example.restapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "users")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    private LocalDateTime createdAt;
+
+    private User(String username, String password, String email, String name) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static User of(String username, String password, String email, String name) {
+        return new User(username, password, email, name);
+    }
+}
