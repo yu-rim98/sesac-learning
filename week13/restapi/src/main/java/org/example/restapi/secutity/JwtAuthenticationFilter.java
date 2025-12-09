68068e435d97d65504492e4b718527331f5addd3
yu-rim98 <rimmm098@gmail.com>
2025-12-09 13:43:53 +0900

    Feat: JwtAuthenticationFilter 정의

---
+++ week13/restapi/src/main/java/org/example/restapi/secutity/JwtAuthenticationFilter.java
@@ -0,0 +1,72 @@
+package org.example.restapi.secutity;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION_STR = "Authorization";
+    private static final String BEARER_STR = "Bearer ";
+
+    private final JwtProvider jwtProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            // 1. 요청에서 JWT 추출
+            String token = resolveToken(request);
+
+            // 2. 토큰 유효성 검사
+            if (StringUtils.hasText(token) && jwtProvider.validateToken(token)) {
+                // 3. 토큰에서 사용자 정보 추출
+                String username = jwtProvider.getUsername(token);
+
+                // 4. UserDetails 가져오기
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                // 5. 인증 객체 생성
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails.getUsername(), null, userDetails.getAuthorities());
+                authentication.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // 6. SecurityContext에 인증 객체 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                System.out.println("인증 완료");
+            }
+
+        } catch (Exception e) {
+            System.out.println("JWT 인증 실패");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    // 헤더에서 토큰 추출(Authorization 필드의 Bearer 접두어 다음)
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_STR);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_STR)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
