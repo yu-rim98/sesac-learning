3c960158cc3eeddd634af5f2a5523530c3561445
yu-rim98 <rimmm098@gmail.com>
2025-12-09 14:14:07 +0900

    Feat: ErrorCode 추가

--- week13/restapi/src/main/java/org/example/restapi/exception/ErrorCode.java
+++ week13/restapi/src/main/java/org/example/restapi/exception/ErrorCode.java
@@ -15,7 +15,11 @@ public enum ErrorCode {
     TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "TODO_NOT_FOUND", "존재하지 않는 Todo입니다."),
 
     // user 에러
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "존재하지 않는 사용자입니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "존재하지 않는 사용자입니다."),
+
+    // auth 에러
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "DUPLICATE_USERNAME", "이미 존재하는 사용자명입니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "INVALID_PASSWORD", "비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
