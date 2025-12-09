a19f95ec1bc10477338a008ccde532c9d8500f4b
yu-rim98 <rimmm098@gmail.com>
2025-12-09 10:25:53 +0900

    Feat: JWT 토큰 발행 테스트

---
+++ week13/restapi/src/main/java/org/example/restapi/controller/JwtTestController.java
@@ -0,0 +1,27 @@
+package org.example.restapi.controller;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.example.restapi.dto.response.ApiResponse;
+import org.example.restapi.secutity.JwtProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jwt-test")
+@RequiredArgsConstructor
+public class JwtTestController {
+
+    private final JwtProvider jwtProvider;
+
+    @GetMapping("/generate")
+    public ResponseEntity<ApiResponse<Map<String, String>>> generateToken(@RequestParam String username) {
+        String token = jwtProvider.createToken(username);
+        return ResponseEntity.ok(ApiResponse.success(Map.of("token", token)));
+    }
+
+//    @GetMapping("/validate")
+}
