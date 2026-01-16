74a3cd8d93dca55d402d569bc1a6bc2c13c84474
yu-rim98 <rimmm098@gmail.com>
2026-01-16 11:26:43 +0900

    Feat: Cors 설정 추가

---
+++ week19/gallery/gallery-back/src/main/java/org/example/galleryback/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package org.example.galleryback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/uploads/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+    }
+}
