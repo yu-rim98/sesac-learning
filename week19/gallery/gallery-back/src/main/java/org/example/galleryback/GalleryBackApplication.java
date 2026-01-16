ac1e364944a4c03b586a62accf33cb8e57fef6d7
yu-rim98 <rimmm098@gmail.com>
2026-01-16 09:13:28 +0900

    project init

---
+++ week19/gallery/gallery-back/src/main/java/org/example/galleryback/GalleryBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.galleryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryBackApplication.class, args);
+    }
+
+}
---
+++ week19/gallery/gallery-back/src/test/java/org/example/galleryback/GalleryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.galleryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
